668b556033a61d5aaf7568ad9bd011134b7e0d90
hrou0003 <54772688+hrou0003@users.noreply.github.com>
2022-05-24 15:30:20 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,3 @@
+const QuestionCard = () => {
+  return <div>Quetstion Card</div>;
+};
